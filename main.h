12b20c22836f987a24f6c2f4846c6ec8926c549c
yousracode23 <yousracoding@gmail.com>
2023-04-11 19:47:07 -0700

    0x0B. C - malloc, free

--- main.h
+++ main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+
 #endif
